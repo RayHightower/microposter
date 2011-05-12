bc665f0a5ff70056a5e192296392cd7bb1354e19
RayHightower <rth@wisdomgroup.com>
2011-05-11 22:16:21 -0500

    additional config for spork support

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module DemoApp
   class Application < Rails::Application
+    # Spork hack added manually by RTH on 5/11/2011 per RoR3T
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
